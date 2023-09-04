80047f737caaa6d925edfa78b344b7da2262803e
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-04 19:35:55 +0530

    made the final changes hopefully

--- ncertExemplar/codes/main.py
+++ ncertExemplar/codes/main.py
@@ -20,10 +20,10 @@ def generate_pmf(size):
 
 pmf_X = generate_pmf(size)
 pmf_Y = generate_pmf(size)
-pmf_W = np.convolve(pmf_X, pmf_Y, mode='full')
+pmf_W_act = np.convolve(pmf_X, pmf_Y, mode='full')
 
 # Calculate the actual probability of getting the same number on both dice (W = 0)
-act_prob_same_no = pmf_W[5]  # Probability at W = 0
+act_prob_same_no = pmf_W_act[5]  # Probability at W = 0
 # Calculate the actual probability of getting different numbers on both dice (|W| > 0)
 act_prob_diff_nos = 1 - act_prob_same_no
 
@@ -34,16 +34,16 @@ Y_simulated = np.random.randint(1, size + 1, num_samples)
 
 # Calculate W for each pair of X and Y
 W_simulated = X_simulated - Y_simulated
+pmf_W_sim = np.bincount(W_simulated + size - 1) / num_samples
 
 # Calculate the simulated probability of getting the same number on both dice (W = 0)
-sim_prob_same_no = np.sum(W_simulated == 0) / num_samples
-
+sim_prob_same_no = pmf_W_sim[5]  # Probability at W = 0
 # Calculate the simulated probability of getting different numbers on both dice (|W| > 0)
-sim_prob_diff_nos = np.sum(W_simulated != 0) / num_samples
+sim_prob_diff_nos = 1 - sim_prob_same_no
 
 print("Actual Probability (W = 0):", act_prob_same_no)
 print("Simulated Probability (W = 0):", sim_prob_same_no)
-#
+
 if np.isclose(act_prob_same_no, act_prob_same_no):
     print("Hence verified for same number on both dice")
 else:
@@ -54,14 +54,13 @@ print("Simulated Probability (W ≠ 0):", sim_prob_diff_nos)
 
 if np.isclose(act_prob_diff_nos, act_prob_diff_nos):
     print("Hence verified for different numbers on both dice")
-
 else:
     print("error")
 
 k_values = np.arange(-size + 1, size) 
    
-plt.stem(k_values, pmf_W, markerfmt='o', linefmt='C0-', use_line_collection=True, label='Actual')
-plt.stem(k_values,np.bincount(W_simulated + size - 1) / num_samples, markerfmt='o', linefmt='C1-', use_line_collection=True, label='Simulation')
+plt.stem(k_values, pmf_W_act, markerfmt='o', linefmt='C0-', use_line_collection=True, label='Actual')
+plt.stem(k_values, pmf_W_sim, markerfmt='o', linefmt='C1-', use_line_collection=True, label='Simulation')
 plt.xlabel("k")
 plt.ylabel("Probability (simulated vs actual)")
 plt.legend()
