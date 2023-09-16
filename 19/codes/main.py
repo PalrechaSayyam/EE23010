cc1d7f0fff14d2c259ea202489a59bfbaf8e75b2
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-16 18:46:58 +0530

    added all the files and updated

--- 19/codes/main.py
+++ 19/codes/main.py
@@ -1,37 +1,69 @@
-import random
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import bernoulli
 
-# Number of simulations
-num_simulations = 100000
+#Probability of the event of getting same number on both dice, i.e., W=0 is 1/6
+prob = 1/6
+size = int(1/prob)
 
-# Initialize an array to store the sum of two card numbers for each simulation
-sums = np.zeros(num_simulations)
+#Number of samples is 10^(size-1), 0 denotes same number on dice
+simlen=int(100000)
 
-# Simulate the scenario
-for i in range(num_simulations):
-    # Create a deck of cards with numbers 1 to 5
-    deck = [1, 2, 3, 4, 5]
-    
-    # Draw two cards without replacement
-    cards_drawn = random.sample(deck, 2)
-    
-    # Calculate the sum of the two card numbers
-    sum_of_cards = sum(cards_drawn)
-    
-    # Store the sum in the array
-    sums[i] = sum_of_cards
+#Generating sample date using Bernoulli r.v.
+data_bern = bernoulli.rvs(size=simlen,p=prob)
 
-# Calculate the simulated mean and variance
-simulated_mean = np.mean(sums)
-simulated_variance = np.var(sums)
+print("Samples generated:",data_bern)
 
-# Theoretical mean and variance (calculated previously)
-theoretical_mean = 6
-theoretical_variance = 3
+def generate_pmf(size):
+	array = np.full(size, prob)
+	return array
 
-# Print the results
-print("Simulated Mean:", simulated_mean)
-print("Theoretical Mean:", theoretical_mean)
-print("Simulated Variance:", simulated_variance)
-print("Theoretical Variance:", theoretical_variance)
+pmf_X = generate_pmf(size)
+pmf_Y = generate_pmf(size)
+pmf_W_act = np.convolve(pmf_X, pmf_Y, mode='full')
 
+# Calculate the actual probability of getting the same number on both dice (W = 0)
+act_prob_same_no = pmf_W_act[5]  # Probability at W = 0
+# Calculate the actual probability of getting different numbers on both dice (|W| > 0)
+act_prob_diff_nos = 1 - act_prob_same_no
+
+# Simulate the random variables X and Y by rolling two dice
+num_samples = simlen
+X_simulated = np.random.randint(1, size + 1, num_samples)
+Y_simulated = np.random.randint(1, size + 1, num_samples)
+
+# Calculate W for each pair of X and Y
+W_simulated = X_simulated - Y_simulated
+pmf_W_sim = np.bincount(W_simulated + size - 1) / num_samples
+
+# Calculate the simulated probability of getting the same number on both dice (W = 0)
+sim_prob_same_no = pmf_W_sim[5]  # Probability at W = 0
+# Calculate the simulated probability of getting different numbers on both dice (|W| > 0)
+sim_prob_diff_nos = 1 - sim_prob_same_no
+
+print("Actual Probability (W = 0):", act_prob_same_no)
+print("Simulated Probability (W = 0):", sim_prob_same_no)
+
+if np.isclose(act_prob_same_no, act_prob_same_no):
+    print("Hence verified for same number on both dice")
+else:
+    print("error")
+
+print("Actual Probability (W ≠ 0):", act_prob_diff_nos)
+print("Simulated Probability (W ≠ 0):", sim_prob_diff_nos)
+
+if np.isclose(act_prob_diff_nos, act_prob_diff_nos):
+    print("Hence verified for different numbers on both dice")
+else:
+    print("error")
+
+k_values = np.arange(-size + 1, size) 
+   
+plt.stem(k_values, pmf_W_act, markerfmt='o', linefmt='C0-', use_line_collection=True, label='Actual')
+plt.stem(k_values, pmf_W_sim, markerfmt='o', linefmt='C1-', use_line_collection=True, label='Simulation')
+plt.xlabel("k")
+plt.ylabel("Probability (simulated vs actual)")
+plt.legend()
+plt.grid()
+plt.savefig('../figs/main_actvssim.png')
+plt.show()
