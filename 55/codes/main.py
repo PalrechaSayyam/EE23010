4b6039d6208d0e136b0a46425d61797b43189a39
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-17 01:32:29 +0530

    hope it works

--- 55/codes/main.py
+++ 55/codes/main.py
@@ -1,21 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Number of simulations and defining the values on cards
 num_simulations = 100000
-cards = [1, 2, 3, 4, 5]
-
-# Initialize array to store simulated values of X
-X_simulated = []
+cards = np.arange(1, 6)
 
 # Simulate the scenario and calculate X for each simulation
-for _ in range(num_simulations):
-    cards = np.arange(1, 6)
-    np.random.shuffle(cards)
-    selected_cards = np.random.choice(cards, size=2, replace=False)
-    X_simulated.append(np.sum(selected_cards))
+selected_indices = np.random.choice(len(cards), size=(num_simulations, 2))
+selected_cards = cards[selected_indices]
 
-X_simulated = np.array(X_simulated)
+# Filter out cases where both cards have the same value
+selected_cards = selected_cards[selected_cards[:, 0] != selected_cards[:, 1]]
+
+X_simulated = np.sum(selected_cards, axis=1)
 
 # Calculate the simulated mean and variance
 simulated_mean = np.mean(X_simulated)
@@ -25,26 +21,17 @@ simulated_variance = np.var(X_simulated)
 combinations = np.array(np.meshgrid(cards, cards)).T.reshape(-1, 2)
 combinations = combinations[combinations[:, 0] != combinations[:, 1]]
 
-# Calculate the PMF using NumPy
-p_X_theoretical, _ = np.histogram(combinations.sum(axis=1), bins=np.arange(2.5, 10.5), density=True)
+# Calculate the PMF using NumPy (Vectorized)
+X_values = np.arange(3, 10)
+count = np.sum(combinations[:, 0] + combinations[:, 1] == X_values[:, np.newaxis], axis=1)
+p_X_theoretical = count / len(combinations)
 
-# Values of X range from 3 to 9
-k_values = np.array(range(3, 10))
-hist_bins = np.arange(2.5, 10.5)
+# Calculate theoretical mean and variance (Vectorized)
+theoretical_mean = np.dot(X_values, p_X_theoretical)
+theoretical_variance = np.dot(X_values**2, p_X_theoretical) - theoretical_mean**2
 
-# Plot the PMFs as a stem plot
-plt.stem(k_values, p_X_theoretical, linefmt='b-', markerfmt='bo', basefmt=' ', label='X_theoretical', use_line_collection=True)
-plt.stem(k_values, np.histogram(X_simulated, bins=hist_bins, density=True)[0], linefmt='r-', markerfmt='ro', basefmt=' ', label='X_simulated', use_line_collection=True)
-plt.xlabel('X')
-plt.ylabel('Probability')
-plt.xticks(k_values)
-plt.legend()
-plt.grid(True)
-plt.savefig('../figs/main.png')
-plt.show()
-
-print("Theoretical Mean:", np.dot(k_values, p_X_theoretical))
+print("Theoretical Mean:", theoretical_mean)
 print("Simulated Mean:", simulated_mean)
-print("Theoretical Variance:", np.dot(k_values**2, p_X_theoretical) - (np.dot(k_values, p_X_theoretical))**2)
+print("Theoretical Variance:", theoretical_variance)
 print("Simulated Variance:", simulated_variance)
 
