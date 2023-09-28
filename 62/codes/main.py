525588df041c87977a7c7da88767a286d2e88e71
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-28 10:20:57 +0530

    added scatter plot

--- 62/codes/main.py
+++ 62/codes/main.py
@@ -1,31 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Number of samples
+sets = 1000
 samples = 10000
 
-samples_sim = np.random.uniform(-np.sqrt(3), np.sqrt(3), size=samples)
+sim_vars = []
 
-sim_var = np.var(samples_sim)
-theo_var = 1  
+theo_var = 1 
 
-x_values = np.linspace(-np.sqrt(3), np.sqrt(3), num=1000)
-simulated_variance = [sim_var if -np.sqrt(3) <= x <= np.sqrt(3) else 0 for x in x_values]
-theoretical_variance = [theo_var if -np.sqrt(3) <= x <= np.sqrt(3) else 0 for x in x_values]
+for _ in range(sets):
+    sim_samples = np.random.uniform(-np.sqrt(3), np.sqrt(3), size=samples)
+    sim_var = np.var(sim_samples)
+    sim_vars.append(sim_var)
 
-# Create the plot
-plt.figure(figsize=(9, 6))
-plt.plot(x_values, simulated_variance, label='Simulated Variance', color='g')
-plt.plot(x_values, theoretical_variance, label='Theoretical Variance', linestyle='--', color='r')
-plt.xlabel('X')
+theo_vars = [theo_var] * sets
+
+# Create the scatter plot
+yRange = [0.85, 1.15]
+plt.figure(figsize=(10, 6))
+plt.scatter(range(sets), sim_vars, label='Simulated Variance', color='b', marker='o', s=10)
+plt.plot(range(sets), theo_vars, label='Theoretical Variance', linestyle='--', color='r')
+plt.xlabel('Simulation Set')
 plt.ylabel('Variance')
-plt.ylim(0, 1.5) 
 plt.title('Simulated vs. Theoretical Variance of X')
+plt.ylim(yRange[0], yRange[1])
 plt.legend()
 plt.grid(True)
 plt.savefig('../figs/main.png')
 plt.show()
-
-print(f"Simulated Variance: {sim_var}")
-print(f"Theoretical Variance: {theo_var}")
 
