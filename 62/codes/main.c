8133f2a47205fc5b07c81573bea04506e5166602
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-29 15:16:01 +0530

    added uniform random numbeers generators

--- 62/codes/main.c
+++ 62/codes/main.c
@@ -1,8 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main () {
-    
-    return 0;
+//Function declaration
+//void gaussian(char *str, int len);
+void uniform(char *str, int len);
+double mean(char *str);
+//End function declaration
+
+//Defining the function for generating uniform random numbers
+void uniform(char *str, int len){
+	int i;
+	FILE *fp;
+	fp = fopen(str,"w");
+	//Generate numbers
+	for (i = 0; i < len; i++){
+		fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+	}
+	fclose(fp);
 }
+
+//End function for generating uniform random numbers
+
+//Defining the function for generating Gaussian random numbers
+
+/*void gaussian(char *str, int len){
+	int i,j;
+	double temp;
+	FILE *fp;
+	fp = fopen(str,"w");
+	//Generate numbers
+	for (i = 0; i < len; i++){
+		temp = 0;
+		for (j = 0; j < 12; j++){
+			temp += (double)rand()/RAND_MAX;
+		}
+		
+	temp-=6;
+	fprintf(fp,"%lf\n",temp);
+	
+	}
+	fclose(fp);
+}*/
+//End function for generating Gaussian random numbers
+
+//Defining the function for calculating the mean of random numbers
+
+double mean(char *str){
+	int i = 0,c;
+	FILE *fp;
+	double x, temp = 0.0;
+	fp = fopen(str,"r");
+	//get numbers from file
+	while(fscanf(fp,"%lf",&x)!=EOF)
+	{
+		//Count numbers in file
+		i=i+1;
+		//Add all numbers in file
+		temp = temp+x;
+	}
+	fclose(fp);
+	temp = temp/(i-1);
+	return temp;
+}
+//End function for calculating the mean of random numbers
+
+
+int main(void)
+//main function begins
+{
+	//Uniform random numbers
+	uniform("uni.dat", 1000000);
+	
+	//Gaussian random numbers
+	//gaussian("gau.dat", 1000000);
+	
+	//Mean of uniform
+	printf("%lf\n",mean("uni.dat"));
+	//printf("%lf\n",mean("gau.dat"));
+	return 0;
+	
+}
+
