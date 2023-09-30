aff6b65dea044268d87e82ff674b6a6fe24d0200
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-09-30 16:52:37 +0530

    --

--- 62/codes/main.py
+++ 62/codes/main.py
@@ -4,8 +4,9 @@ import sympy as sp
 
 samples = 10000
 x = sp.symbols('x')
+prob = (1 / (2 * sp.sqrt(3)))
 
-f_x = (1 / (2 * sp.sqrt(3))) * sp.Piecewise((1, (x >= -sp.sqrt(3)) & (x <= sp.sqrt(3))), (0, True))
+f_x = prob * sp.Piecewise((1, (x >= -sp.sqrt(3)) & (x <= sp.sqrt(3))), (0, True))
 theo_var = sp.integrate(x**2 * f_x, (x, -sp.oo, sp.oo))
 
 sim_samples = np.random.uniform(-np.sqrt(3), np.sqrt(3),size=samples)
