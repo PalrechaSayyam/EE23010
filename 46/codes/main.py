242d2d68f88e15fcebbe708dc5871104ed330418
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-10-06 20:41:55 +0530

    added graph

--- 46/codes/main.py
+++ 46/codes/main.py
@@ -1,4 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def cdf(x):
+    if x < -1:
+        return 0
+    elif x >= -1 and x < 0:
+        return (1/4) * (x + 1)
+    elif x >= 0 and x < 1:
+        return (1/4) * (x + 3)
+    elif x >= 1:
+        return 1
 
 def pdf(u):
     if u < -1:
@@ -12,7 +23,7 @@ def pdf(u):
     elif u >= 1:
         return 0
 
-num_sim = 1000000
+num_sim = 10000
 
 prob_zero = pdf(0)
 
@@ -38,9 +49,24 @@ desired = X[(X > lb) & (X < ub)]
 sim_prob = len(desired)/len(X)
 act_prob = 5/8
 
-if abs(sim_prob - act_prob) <= 0.001:
+if abs(sim_prob - act_prob) < 0.005:
     print("Statement (B) is true.")
 else:
     print("Statement (B) is not true.")
 
 print(f"Calculated probability: {sim_prob}")
+
+x_values = np.linspace(-1, 1, 1000)
+
+theo_cdf = [cdf(x) for x in x_values]
+sorted_X = np.sort(X)
+sim_cdf = np.arange(1, num_sim+1) / num_sim
+
+plt.plot(sorted_X, sim_cdf, label='Simulated CDF')
+plt.plot(x_values, theo_cdf, label='Theoretical CDF', linestyle='--')
+plt.legend()
+plt.xlabel('X')
+plt.ylabel('CDF')
+plt.title('Simulated vs Theoretical CDF')
+plt.savefig('../figs/main.png')
+#plt.show()
