423fdadbe19220fcc0d5d17a1a8c7852fb530bf3
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-10-14 15:10:04 +0000

    added random player prototype

--- audio-player/backend/server.py
+++ audio-player/backend/server.py
@@ -1,6 +1,19 @@
 from flask import Flask, send_file
+import random
+import os
 
 app = Flask(__name__)
+
+audio_directory = '/storage/self/primary/Download/EE23010/audio-player/audio'
+
+@app.route('/play-random-audio')
+def play_random():
+    audio_files = os.listdir(audio_directory)
+    uni_dist = [random.uniform(0,1) for _ in range(len(audio_files))]
+    indices = [int(rand_no*len(audio_files)) for rand_no in uni_dist]
+    random_file = audio_files[indices[0]]
+    audio_path = os.path.join(audio_directory, random_file)
+    return send_file(audio_path)
 
 @app.route('/audio/<filename>')
 def serve_audio(filename):
