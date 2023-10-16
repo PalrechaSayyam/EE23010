aa277f7b1fa1a1841f40bef49703830997110e0c
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-10-16 20:42:55 +0530

    updated frontend-fixed

--- audio-player/backend/server.py
+++ audio-player/backend/server.py
@@ -13,22 +13,12 @@ def play_random():
     indices = [int(rand_no*len(audio_files)) for rand_no in uni_dist]
     random_file = audio_files[indices[0]]
     audio_path = os.path.join(audio_directory, random_file)
-    return send_file(audio_path)
+    return render_template('front.html')
 
 @app.route('/audio/<filename>')
 def serve_audio(filename):
     audio_path = f'/storage/self/primary/Download/EE23010/audio-player/audio/{filename}'
     return send_file(audio_path)
-
-@app.route('/pages/<filename>')
-def serve_html(filename):
-    html_path = f'/storage/self/primary/Download/EE23010/audio-player/src/pages/{filename}'
-    return send_file(html_path)
-
-@app.route('/components/<filename>')
-def serve_css(filename):
-    css_path = f'/storage/self/primary/Download/EE23010/audio-player/src/components/{filename}'  # Update the path to your CSS file
-    return send_file(css_path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
