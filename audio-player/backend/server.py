a9a5659250565447c9817f77e085c5767ddf806e
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-10-23 09:33:07 +0000

    corrected issues...

--- audio-player/backend/server.py
+++ audio-player/backend/server.py
@@ -3,19 +3,21 @@ import random
 import os
 import base64
 
-app = Flask(__name__, template_folder='/storage/self/primary/Download/EE23010/audio-player/src',static_folder='/storage/self/primary/Download/EE23010/audio-player/src')
+app = Flask(__name__, template_folder='/storage/self/primary/Download/EE23010/audio-player/src', static_folder='/storage/self/primary/Download/EE23010/audio-player/src')
 
 audio_directory = '/storage/self/primary/Download/EE23010/audio-player/audio'
 
 @app.route('/play-random-audio')
 def play_random():
     audio_files = os.listdir(audio_directory)
+    return render_template('pages/front.html', audio_files=audio_files)
+
+@app.route('/get-shuffled-indices')
+def get_shuffled_indices():
+    audio_files = os.listdir(audio_directory)
     uni_dist = [random.uniform(0, 1) for _ in range(len(audio_files))]
     indices = [int(rand_no * len(audio_files)) for rand_no in uni_dist]
-    random_file = audio_files[indices[0]]
-    with open(os.path.join(audio_directory, random_file), 'rb') as audio_file:
-        base_data = base64.b64encode(audio_file.read()).decode('utf-8')
-        return render_template('pages/front.html',base64_audio_data=base_data,audio_files=audio_files)
+    return indices
 
 @app.route('/get-audio-list')
 def get_audio_list():
